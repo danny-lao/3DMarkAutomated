b0868e945d87fadf773bda7305d5506873ad2e3c
KonDaii <dannylao1101@gmail.com>
2024-05-03 15:54:30 -0400

    Locating Stress Tests
    Locate stress test via Pyautogui and Opencv-python

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import subprocess
+import pyautogui
+import time
+
 
 subprocess.call(r"C:\Program Files (x86)\Steam\steam.exe -applaunch 223850")
+time.sleep(45)
+
+pyautogui.PAUSE=2.5
+#find stressTest button on 3DMark screen
+x,y = pyautogui.locateCenterOnScreen("stressTest.png", confidence=0.7)
+pyautogui.moveTo(x,y,1)
+pyautogui.click()
 
